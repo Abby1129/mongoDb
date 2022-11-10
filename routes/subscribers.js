71a4d16386640ea4b27335a651dcb67ae5a7b751
Abby F <abby_faigao@yahoo.com>
2022-11-09 22:05:14 -0800

    create getSub middleware

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
 //Getting all
-router.get("/", (req, res) => {
-  res.send("Hello World");
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //Getting one
@@ -12,12 +18,38 @@ router.get("/:id", (req, res) => {
 });
 
 //Creating one
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribeToChannel: req.body.subscribeToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Updating one
 router.patch("/:id", (req, res) => {});
 
 //Deleting one
 router.delete("/:id", (req, res) => {});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(400).json({ message: "Cannot find subscriber" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
