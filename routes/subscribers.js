cd59926768f60c2240ce27f6d186ad88cc28cea5
Abby F <abby_faigao@yahoo.com>
2022-11-10 07:50:35 -0800

    use middleware and update routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,15 +13,15 @@ router.get("/", async (req, res) => {
 });
 
 //Getting one
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
 });
 
 //Creating one
 router.post("/", async (req, res) => {
   const subscriber = new Subscriber({
     name: req.body.name,
-    subscribeToChannel: req.body.subscribeToChannel,
+    subscribedToChannel: req.body.subscribedToChannel,
   });
   try {
     const newSubscriber = await subscriber.save();
@@ -32,10 +32,30 @@ router.post("/", async (req, res) => {
 });
 
 //Updating one
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Deleting one
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted Subscriber" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 async function getSubscriber(req, res, next) {
   let subscriber;
